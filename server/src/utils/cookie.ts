a9912e600861f73b34f19cf722656328a2d0f8c1
Ubuntu <ubuntu@ip-172-31-42-6.ap-northeast-2.compute.internal>
2022-01-08 09:42:40 +0000

    setup complete for code-server

--- server/src/utils/cookie.ts
+++ server/src/utils/cookie.ts
@@ -3,8 +3,7 @@ import { Response } from "express";
 export const setCookie = (res: Response, token: string) => {
   res.cookie("token", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === "production",
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
     maxAge: 3600 * 1000,
     domain: process.env.BASE_DOMAIN,
